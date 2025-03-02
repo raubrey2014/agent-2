490dd6618af84fc68e1c18f0b1e0e7404dcf0236
Ryan Aubrey <ryanmitchellaubrey@gmail.com>
2025-03-02 15:18:42 -0500

    Attempt to fix build errors

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   env: {
     DATABASE_URL: process.env.DATABASE_URL,
+    DATABASE_URL_UNPOOLED: process.env.DATABASE_URL_UNPOOLED,
   },
 };
 
